f8fa293aec8c8accd986f6978d9196026e1a21fb
Khalipe <VJK1871997@gmail.com>
2024-06-11 13:43:32 +0530

    consumer api

---
+++ kafka-consumer-api/src/main/java/com/prowings/kafkaconsumerapi/service/KafkaConsumerService.java
@@ -0,0 +1,13 @@
+package com.prowings.kafkaconsumerapi.service;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumerService {
+
+	@KafkaListener(topics = "test", groupId = "group_id")
+	public void consume(String message) {
+		System.out.println(">>>>>>>>>> Consumed message: " + message);
+	}
+}
